f0f69be2a6af69781e752257df8b87069515bb5d
xAeternalis <antoine.holzapfel@laposte.net>
2023-03-28 19:18:25 +0200

    Zeniva Adding Enemy Adding events key for player Adding movement for Enemy

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from gameObject import GameObject
 from player import Player
+from enemy import Enemy
 
 
 class Game:
@@ -21,6 +22,7 @@ class Game:
         self.game_window = pygame.display.set_mode((self.width, self.height))
 
         self.player = Player(375, 700, 50, 50, 'assets/player.png', 10)
+        self.enemy = Enemy(0, 600, 50, 50, 'assets/enemy.png', 10)
 
     def draw_objects(self):
         self.game_window.fill(self.white_colour)
@@ -28,15 +30,28 @@ class Game:
         self.game_window.blit(self.background.image, (self.background.x, self.background.y))
         self.game_window.blit(self.treasure.image, (self.treasure.x, self.treasure.y))
         self.game_window.blit(self.player.image, (self.player.x, self.player.y))
+        self.game_window.blit(self.enemy.image, (self.enemy.x, self.enemy.y))
 
         pygame.display.update()
 
     def run_game_loop(self):
+        player_direction = 0
+
         while True:
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     return
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        player_direction = -1
+                    elif event.key == pygame.K_DOWN:
+                        player_direction = 1
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        player_direction = 0
 
+            self.player.move(player_direction, self.height)
+            self.enemy.move(self.width)
             self.draw_objects()
             self.clock.tick(60)
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from gameObject import GameObject
 
+
 class Player(GameObject):
 
     def __init__(self, x, y, width, height, image_path, speed):
@@ -8,4 +9,6 @@ class Player(GameObject):
         self.speed = speed
 
     def move(self, direction, max_height):
+        if (self.y >= max_height - self.height and direction > 0) or (self.y <= 0 and direction < 0):
+            return
         self.y += (direction * self.speed)
